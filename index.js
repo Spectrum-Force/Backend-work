fc153b1855f1c22de05aabf212d57f512e23ff43
Ehoneah Frank <ehoneahfrank2017@gmail.com>
2024-07-01 15:18:40 +0000

    Created User model and Schema

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+// Connect to database
+await mongoose.connect(process.env.MONGO_URL);
+
+dbConnection()
+
+// create Express App
+const app = express();
+
+
+
+
+// Listem for incoming requests
+app.listen(3000, () =>{
+    console.log('App is listening on port 3000')
+});
---
+++ models/event.js
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+     email: {
+        type: String,
+        required: true,
+        unique: true
+
+     },
+     
+     password: {
+        type: String,
+        required: true,
+        minlength: 8,
+     },
+
+}, {timestamps: true});
+
+const User = model ('User', userSchema);
+
+export default User;

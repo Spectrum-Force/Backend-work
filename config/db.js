ec8a50d9354f4fe8b53794219bf33d95eef648e9
Ehoneah Frank <ehoneahfrank2017@gmail.com>
2024-07-04 09:24:14 +0000

    made changes to the db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import 'dotenv/config';
 
 
-const connectionstring = process.env.Mongo_URL;
+const connectionstring = process.env.MONGO_URL;
 
 export const dbConnection = () =>{
     mongoose.connect(connectionstring).then(() =>{
